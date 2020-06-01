998225b1eb1c38bf8ca245260cd55fb494b8e3ee
brock <brock@pop-os.localdomain>
2020-05-31 19:18:15 -0700

    Adds CORS handling to backend server

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 // imports
 const express = require('express')
 const routes = require('./routes')
+const cors = require('cors')
 
 const port = process.env.PORT || 3001
 const app = express()
 
 // middleware - JSON parsing
 app.use(express.json())
+
+// middleware - cors
+app.use(cors())
 
 // middleware - API routes
 app.use('/api/v1/games', routes.games)
