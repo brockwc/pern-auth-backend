22598b61a9e0212436143ebe0a15353d877639d6
lucyefranco <lucyefranco@gmail.com>
2020-11-17 12:28:21 -0800

    begins to set up routes, pulling from models, and seeding

---
+++ seeders/20201117201637-breweries.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+      // const bulkBreweries = await queryInterface.bulkInsert('breweries', [
+        // {
+          // 
+        // }
+      // ])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
