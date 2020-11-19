239549c5b32f1e74055b149e59a38a36f1367eb2
tbisho <tess.bishop8@gmail.com>
2020-11-19 15:50:52 -0700

    creates CRUD functionality for FIND, CREATE, UPDATE, DELETE a profile

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,38 +1,81 @@
-//require models
 const db = require('../models')
-// const url = `http://localhost:4000/api/v1`
-
-//function to render all profiles
-const allProfiles = (req,res) => {
-    db.profile.findAll().then((foundProfiles) => {
-        if(!foundProfiles) return res.json({
-            message: 'No profiles in your database'
-        })
-        res.status(200).json({ profiles: foundProfiles})
+// GET request for all profiles
+const allProfiles = (req, res) => {
+  db.profile.findAll().then((foundProfiles) => {
+    if (!foundProfiles) return res.json({
+      message: 'No profiles in Database'
     })
+    res.status(200).json({ profiles: foundProfiles })
+  })
 }
-    
-
-//function to show just one profile
-const userProfile = (req,res) => {
-    db.profile.findOrCreate({
-        where: {
-            userId: req.params.id
-        }
-    }).then((profile) => {
-        res.status(200).json({ profile })
+//GET request for finding one profile
+const getProfile = (req, res) => {
+  db.profile.findOne({
+    where: { userId: req.params.id }
+  }).then((profile) => {
+    res.status(200).json({ profile })
+  })
+}
+//POST request for creating a profile if it doesn't exist
+const createProfile = (req, res) => {
+  db.profile.findOrCreate({
+    where: {
+      userId: req.params.id,
+      display_name: "Elma Jenkins",
+      gender: "female",
+      image: "",
+      city: "New York",
+      state: "New York",
+      about_me: "I am sweet and patient"
+    }
+  }).then((profile) => {
+    res.status(200).json({ profile })
+  })
+}
+// const updateProfile = (req, res) => {
+//   db.profile.update({
+//     ...req.body
+//   }, {
+//     where: {
+//       userId: req.params.id
+//     }
+//   }).then((updatedProfile) => {
+//     if (!updatedProfile) return res.json({
+//       message: `No profile with id ${req.params.id} found.`
+//     })
+//     res.status(200).json({profile: updatedProfile })
+//   })
+// }
+const updateProfile = (req, res) => {
+  db.profile.update({
+    city: "San Francisco",
+    state: "California",
+  }, {
+    where: {
+      userId: req.params.id
+    }
+  }).then((updatedProfile) => {
+    if (!updatedProfile) return res.json({
+      message: `No profile with id ${req.params.id} found.`
     })
-
+    res.status(200).json({ profile: updatedProfile })
+  })
 }
-
-//function to create
-
-//function to update
-
-//function to destroy 
-
-//export modules
+//DELETE request to remove a profile
+const removeProfile = (req, res) => {
+  db.profile.destroy({
+    where:
+      { userId: req.params.id }
+  }).then(() => {
+    res.json({ message: `Profile with id ${req.params.id} deleted.` })
+  }).catch(err => {
+    res.json({ message: err })
+  })
+}
 module.exports = {
-    allProfiles,
-    userProfile
+  allProfiles,
+  getProfile,
+  createProfile,
+  removeProfile,
+  updateProfile
 }
--- routes/profile.js
+++ routes/profile.js
@@ -6,7 +6,10 @@ const ctrl = require('../controllers')
 // GET -- we need a route to view all profiles in the browse page
 router.get('/', ctrl.profile.allProfiles)
 // GET -- we need a route to view a single profile
-router.get('/:id', ctrl.profile.userProfile)
+router.get('/:id', ctrl.profile.getProfile)
+router.post('/:id', ctrl.profile.createProfile)
+router.put('/:id', ctrl.profile.updateProfile)
+router.delete('/:id', ctrl.profile.removeProfile)
 // POST -- we need a route to create a profile
 // POST -- update profile
 // delete -- delete profile 
