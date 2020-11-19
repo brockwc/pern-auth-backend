ac6d6ac5a1d817bcf1f3ddc96efb5bacd57844a3
astudillome <projectagility@gmail.com>
2020-11-19 15:51:28 -0700

    creates CRUD functionality: create, read, update and delete a profile

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,25 +1,87 @@
 const db = require('../models')
-
+// GET request for all profiles
 const allProfiles = (req, res) => {
   db.profile.findAll().then((foundProfiles) => {
-    if(!foundProfiles) return res.json({
+    if (!foundProfiles) return res.json({
       message: 'No profiles in Database'
     })
-    res.status(200).json({ profiles: foundProfiles})
+    res.status(200).json({ profiles: foundProfiles })
   })
 }
 
-const userProfile = (req, res) => {
-  db.profile.findOrCreate({
-    where: {userId: req.params.id}
+//GET request for finding one profile 
+const getProfile = (req, res) => {
+  db.profile.findOne({
+    where: { userId: req.params.id }
   }).then((profile) => {
     res.status(200).json({ profile })
   })
 }
 
+//POST request for creating a profile if it doesn't exist
+const createProfile = (req, res) => {
+  db.profile.findOrCreate({
+    where: {
+      userId: req.params.id,
+      display_name: "Elma Jenkins",
+      gender: "female",
+      image: "",
+      city: "New York",
+      state: "New York",
+      about_me: "I am sweet and patient"
+    }
+  }).then((profile) => {
+    res.status(200).json({ profile })
+  })
+}
 
+// const updateProfile = (req, res) => {
+//   db.profile.update({
+//     ...req.body
+//   }, {
+//     where: {
+//       userId: req.params.id
+//     }
+//   }).then((updatedProfile) => {
+//     if (!updatedProfile) return res.json({
+//       message: `No profile with id ${req.params.id} found.`
+//     })
+//     res.status(200).json({profile: updatedProfile })
+//   })
+// }
+
+const updateProfile = (req, res) => {
+  db.profile.update({
+    city: "San Francisco",
+    state: "California",
+  }, {
+    where: {
+      userId: req.params.id
+    }
+  }).then((updatedProfile) => {
+    if (!updatedProfile) return res.json({
+      message: `No profile with id ${req.params.id} found.`
+    })
+    res.status(200).json({ profile: updatedProfile })
+  })
+}
+
+//DELETE request to remove a profile
+const removeProfile = (req, res) => {
+  db.profile.destroy({
+    where:
+      { userId: req.params.id }
+  }).then(() => {
+    res.json({ message: `Profile with id ${req.params.id} deleted.` })
+  }).catch(err => {
+    res.json({ message: err })
+  })
+}
 
 module.exports = {
   allProfiles,
-  userProfile
+  getProfile,
+  createProfile,
+  removeProfile,
+  updateProfile
 }
--- routes/profile.js
+++ routes/profile.js
@@ -2,6 +2,12 @@ const router = require('express').Router()
 const ctrl = require('../controllers');
 
 router.get('/', ctrl.profile.allProfiles);
-router.get('/:id', ctrl.profile.userProfile);
+router.get('/:id', ctrl.profile.getProfile);
+
+router.post('/:id', ctrl.profile.createProfile);
+
+router.put('/:id', ctrl.profile.updateProfile);
+
+router.delete('/:id', ctrl.profile.removeProfile);
 
 module.exports = router
