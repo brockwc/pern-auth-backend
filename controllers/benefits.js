ea30eeb9fe7fe62dc7a1382609afc258d78615a0
brockmolmen <brockmolmen@gmail.com>
2020-11-18 20:28:52 -0800

    commit before merge, trying to fix my junk

--- controllers/benefits.js
+++ controllers/benefits.js
@@ -2,15 +2,6 @@ const db = require('../models')
 
 // Find all Teas
 const index = (req, res) => {
-<<<<<<< HEAD
-    db.benefit.findAll().then((foundBenefits) => {
-        if (!foundBenefits) return res.json({
-            message: 'No benefits in db'
-        })
-        res.send('Incomplete benefits#index controller function')
-    })
-        .catch(err => console.log('error at benefits#index', err))
-=======
     console.log('contollers/benefits')
     db.benefit.findAll().then((foundBenefits) => {
         console.log('query complete')
@@ -20,7 +11,6 @@ const index = (req, res) => {
         res.send(foundBenefits)
     })
         .catch(err => console.log('error at Benefits#index', err))
->>>>>>> d0a6b6db29f740ad81ded636a6d96c2035303535
 }
 
 module.exports = {
