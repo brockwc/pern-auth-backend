04bf5316957449ecf23933ac62e3d948636d5ddd
jmcho311 <jmcho311@gmail.com>
2020-11-20 16:09:54 -0800

    beer controllers and routes created and tested

--- controllers/beer.js
+++ controllers/beer.js
@@ -1,4 +1,5 @@
 const db = require('../models')
+const { Op } = require("sequelize")
 
 //Find all of the brewery posts
 const index = (req, res) => {
@@ -12,53 +13,73 @@ const index = (req, res) => {
 }
 
 const show = (req, res) => {
+    // if(req.params.category === )...ignore special characters
+    console.log('*******'+req.params.category)
     db.beer.findAll({
         where: {
-            category: req.params
+            category: {
+                [Op.iLike]: `${req.params.category}`
+            }
+        }
+    }).then((foundBeers) => {
+        if(!foundBeers) return res.json({
+            message: 'Beers with selected category not found.'
+        })
+        res.status(200).json({ beers: foundBeers })
+    })
+}
+
+
+const showPost = (req, res) => {
+    console.log(req.params.id)
+    db.beer.findAll({
+        where: {
+            userId: req.params.id
         }
     }).then((foundBeer) => {
         if(!foundBeer) return res.json({
-            message: 'Beers with selected category not found.'
+            message: 'Beer posts with selected user not found.'
         })
         res.status(200).json({ beer: foundBeer })
     })
 }
 
-const create = (req, res) => {
-    db.beer.create(req.body).then((savedBeer) => {
-        // Validations and error handling here
-        res.status(200).json({ beer: savedBeer })
-    })
-}
+// const create = (req, res) => {
+//     db.beer.create(req.body).then((savedBeer) => {
+//         // Validations and error handling here
+//         res.status(200).json({ beer: savedBeer })
+//     })
+// }
 
-const update = (req, res) => {
-    db.beer.update({
-        ...req.body
-    }, {
-        where: {
-            id: req.params.id
-        }
-    }).then((updatedBeer) => {
-        if (!updatedBeer) return res.json({
-            message: "No beer with that ID found."
-        })
-        // Validations and error handling here
-        res.status(200).json({ beer: updatedBeer })
-    })
-}
+// const update = (req, res) => {
+//     db.beer.update({
+//         ...req.body
+//     }, {
+//         where: {
+//             id: req.params.id
+//         }
+//     }).then((updatedBeer) => {
+//         if (!updatedBeer) return res.json({
+//             message: "No beer with that ID found."
+//         })
+//         // Validations and error handling here
+//         res.status(200).json({ beer: updatedBeer })
+//     })
+// }
 
-const destroy = (req, res) => {
-    db.beer.destroy({
-        where: { id: req.params.id }
-    }).then(() => {
-        res.status(200)
-    })
-}
+// const destroy = (req, res) => {
+//     db.beer.destroy({
+//         where: { id: req.params.id }
+//     }).then(() => {
+//         res.status(200)
+//     })
+// }
 
 module.exports = {
     index,
     show,
-    create,
-    update,
-    destroy,
+    showPost,
+    // create,
+    // update,
+    // destroy,
 }
--- routes/beer.js
+++ routes/beer.js
@@ -4,10 +4,11 @@ const ctrl = require('../controllers/beer')
 
 // PATH = /api/v1/auth
 router.get('/', ctrl.index)
-router.get('/:category', ctrl.show)
-router.post('/', ctrl.create)
-router.put('/:id', ctrl.update)
-router.delete('/:id', ctrl.destroy)
+router.get('/category/:category', ctrl.show)
+router.get('/:id', ctrl.showPost)
+// router.post('/', ctrl.create)
+// router.put('/:id', ctrl.update)
+// router.delete('/:id', ctrl.destroy)
 
 
 module.exports = router
