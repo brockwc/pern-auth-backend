d9bf94dac1c2e1d10fd064d5f1255adaf7fb712c
astudillome <projectagility@gmail.com>
2020-11-21 15:56:27 -0700

    fixes profile controller funcionality

--- routes/profile.js
+++ routes/profile.js
@@ -1,14 +1,17 @@
 const router = require('express').Router()
 const ctrl = require('../controllers');
 
-router.get('/', ctrl.profile.getAllProfiles);
+router.get('/browse', ctrl.profile.getAllProfiles);
 
-router.get('/:id', ctrl.profile.getProfile);
+router.get('/', ctrl.profile.getOwnProfile);
+
+router.get('/:id', ctrl.profile.viewProfile);
 
 router.post('/:id', ctrl.profile.createProfile);
 
 router.put('/:id', ctrl.profile.updateProfile);
 
 router.delete('/:id', ctrl.profile.removeProfile);
+
 
 module.exports = router
