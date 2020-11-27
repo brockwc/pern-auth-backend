df436eae1ab58c7e86b98a58fd3d585eed9902e6
laurennguyen395 <laurennguyen395@gmail.com>
2020-11-26 17:39:51 -0800

    added a show all users route

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router()
 const ctrl = require('../controllers')
 
-router.get('/:id', ctrl.user.index)
+router.get('/', ctrl.user.index)
+router.get('/:id', ctrl.user.show)
 router.put('/update', ctrl.user.update)
 
 module.exports = router
